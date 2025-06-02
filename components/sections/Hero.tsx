1a13800deb9331cda8eb66f88a4e99d4e296541d
mac m3 <macm3@macs-MacBook-Pro.local>
2025-06-02 14:06:45 -0700

    remove null and add div to hero on main page

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -44,7 +44,9 @@ export default function Hero({
           >
             {data.title}
           </HeroTitleFadeIn>
-        ) : null}
+        ) : (
+          <div></div>
+        )}
       </div>
     </main>
   );
