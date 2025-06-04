3b46b1437cf5919720b7d18c37672cbcd061c75a
mac m3 <macm3@macs-MacBook-Pro.local>
2025-06-04 15:00:42 -0700

    add inst feels

--- components/sections/Latest.tsx
+++ components/sections/Latest.tsx
@@ -13,11 +13,11 @@ export default function Latest({ data }: Props) {
     <section className="w-full min-h-screen bg-white-gris py-[40px]">
       <div className="px-[16px] md:px-[40px]">
         <SubTitleLine title={data.sub_title} />
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 ">
           {data.instagram_links.map((link, i) => (
             <InstagramEmbed key={i} url={link} />
           ))}
-        </div> */}
+        </div>
       </div>
     </section>
   );
