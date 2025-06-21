2b2327b5feab30fd2b4c8309957d715b6af5f868
mac m3 <macm3@macs-MacBook-Pro.local>
2025-06-21 01:00:53 -0700

    remove lenisprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ScrollToTop />
-        <LenisProvider />
+        {/* <LenisProvider /> */}
         {children}
       </body>
     </html>
