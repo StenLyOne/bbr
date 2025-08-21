e2b8c1164e0cbe258a0194db6b0ec29d2cead8c6
StenLyOne <146474351+StenLyOne@users.noreply.github.com>
2025-08-21 03:34:25 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'bbrgroup.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'www.gobiz.click', pathname: '/**' },
-      { protocol: 'https', hostname: 'gobiz.click', pathname: '/**' },
     ],
   },
 };
