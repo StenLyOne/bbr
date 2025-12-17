f428a007c2735ed95d6d24ee78676921f6801976
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2025-12-17 16:38:02 +0100

    image cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,26 @@
-
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'bbrgroup.net', pathname: '/**' },
-      { protocol: 'https', hostname: 'www.gobiz.click', pathname: '/**' },
-      { protocol: 'https', hostname: 'gobiz.click', pathname: '/**' },
+      { protocol: "https", hostname: "bbrgroup.net", pathname: "/**" },
+      { protocol: "https", hostname: "www.gobiz.click", pathname: "/**" },
+      { protocol: "https", hostname: "gobiz.click", pathname: "/**" },
     ],
+    minimumCacheTTL: 31536000,
+    formats: ["image/avif", "image/webp"],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/_next/image",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
   },
 };
 
